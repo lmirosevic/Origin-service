b040e806aa27f088e1ca4dab0eb1b62ca88805b2
Luka Mirosevic <luka@goonbee.com>
2014-06-05 00:40:27 +0200

    Initial commit with in memory implementation nearly there

---
+++ client.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+var nconf = require('nconf'),
+    zmq = require('zmq'),
+    _ = require('underscore');
+
+
+
+var socket = zmq.socket('dealer');
+
+
+socket.connect('tcp://localhost:56301');
+
+socket.send('hello');
+
+socket.on('message', function(data) {
+  console.log(data);
+});
---
+++ server.js
@@ -0,0 +1,10 @@
+//
+//  server.js
+//  origin-service
+//
+//  Created by Luka Mirosevic on 05/06/2014.
+//  Copyright (c) 2014 Goonbee. All rights reserved.
+//
+
+// Compatibility layer for AWS OpsWorks
+require('./app');
